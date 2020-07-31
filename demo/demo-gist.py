9ce9c8bb6247ddfced2e11211d2c1068fce7afc2
mdcb <mdcb808@gmail.com>
2020-07-31 14:18:00 -0700

    updated to use astropy.io.fits split gist and non gist demos

--- demo/demo-gist.py
+++ demo/demo-gist.py
@@ -5,6 +5,11 @@ import numpy as np
 from astropy.io import fits
 from iqe import iqe
 
+import gist
+def plot_cross(x,y,sz=1,color='red'):
+  gist.pldj([x],[y-sz],[x],[y+sz],color=color)
+  gist.pldj([x-sz],[y],[x+sz],[y],color=color)
+
 filename=f'{os.path.abspath(os.path.dirname(__file__))}/ascam1_20080710T230802.fits'
 data=fits.open(filename)[0].data
 
@@ -13,25 +18,32 @@ z=data[686-4-1-5-5:686+4-1+5-5,711-4-1-1-5:711+4-1-1+5]
 
 #z=numpy.flipud(z)
 ana=iqe(z)
-print(ana)
-
+gist.fma()
+gist.pli(z)
+plot_cross(ana[0]+.5,ana[1]+.5)
 
 # sub-image
 Z = z[10:18,6:14].copy()
 ana=iqe(Z)
-print(ana)
+gist.fma()
+gist.pli(Z)
+plot_cross(ana[0]+.5,ana[1]+.5)
 
 # add hotspot
 Z = z[10:18,6:14].copy()
 Z[3,2]=10.*Z.max()
-ana=iqe(Z)
-print(ana)
+anah=iqe(Z)
+gist.fma()
+gist.pli(Z)
+plot_cross(anah[0]+.5,anah[1]+.5)
 
 # mask hotspot
 msk=np.ones_like(Z)
 msk[3,2]=0.0
-ana=iqe(Z,msk)
-print(ana)
+#gist.pli(msk*Z)
+anam=iqe(Z,msk)
+plot_cross(anam[0]+.5,anam[1]+.5,color='magenta')
+
 
 
 #ar=686-4-1-5-5
